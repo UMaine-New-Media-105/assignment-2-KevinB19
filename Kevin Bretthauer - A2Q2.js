ec0fa5584a6beb8cc0f3bcbe24f107d6ff6df90f
KevinB19 <112514118+KevinB19@users.noreply.github.com>
2023-03-03 20:58:26 -0500

    Update Kevin Bretthauer - A2Q2.js

--- Kevin Bretthauer - A2Q2.js
+++ Kevin Bretthauer - A2Q2.js
@@ -5,13 +5,18 @@ function setup() {
 function draw() {
   background(220); 
 
-  fill(255, 0, 0); // sets the fill color of the heart to red
-
-  beginShape(); // starts defining the shape of the heart using vertices
-  vertex(200, 100); // top point of the heart
-  bezierVertex(200, 75, 150, 75, 150, 100); // top left curve of the heart
-  bezierVertex(150, 125, 200, 175, 200, 175); // bottom left curve of the heart
-  bezierVertex(200, 175, 250, 125, 250, 100); // bottom right curve of the heart
-  bezierVertex(250, 75, 200, 75, 200, 100); // top right curve of the heart
-  endShape(); // completes the shape of the heart
+  fill('red');
+  noStroke()
+  beginShape(); // starts defining the shape of the star using vertices
+  vertex(200, 50); // top point
+  vertex(225, 125); // top right point
+  vertex(300, 125); // right point
+  vertex(250, 175); // bottom right point
+  vertex(275, 250); // bottom point
+  vertex(200, 200); // center point
+  vertex(125, 250); // bottom left point
+  vertex(150, 175); // bottom left point
+  vertex(100, 125); // left point
+  vertex(175, 125); // top left point
+  endShape(); // completes the shape of the star
 }
